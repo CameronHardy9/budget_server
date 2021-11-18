d84d0c65d59b630c5e579bb120a80f6b9f988c03
Cameron Hardy <camhardy99@gmail.com>
2021-11-18 13:49:57 -0600

    inital commit with server/mongodb setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+MongoClient.connect(process.env.MONGO_DB_URI, {promiseLibrary: Promise}, (err, client) => {
+    if(err) {
+        console.error(err)
+    }
+    app.locals.client = client;
+    app.listen(port, () => {
+        console.log('Listening on port', port);
+    });
+});
